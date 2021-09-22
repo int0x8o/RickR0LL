d20153fcc463579a4db1afa8c74a5f217b0ce088
int0x8o <91141371+int0x8o@users.noreply.github.com>
2021-09-22 22:49:15 +0800

    Add files via upload
    Run this for the installation of the required package for the rickroll scripts

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+yum -y install wget
+
+yum -y install gcc openssl-devel bzip2-devel libffi-devel zlib-devel xz-devel 
+cd /usr/src  
+
+wget https://www.python.org/ftp/python/3.7.11/Python-3.7.11.tgz
+tar xzf Python-3.7.11.tgz 
+
+cd Python-3.7.11 
+./configure --enable-optimizations 
+make altinstall 
+
+rm -f /usr/src/Python-3.7.11.tgz 
+
+ln -s /usr/local/bin/python3.7 /usr/bin/python3.7
+ln -s /usr/local/bin/pip3.7 /usr/bin/pip3.7
+
+
+clear 
+
+python3.7 -V 
+which python3.7
+
+
